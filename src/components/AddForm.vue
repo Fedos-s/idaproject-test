e98b1ff0a52a7e1268a0d8dd9c3817e995fbfe03
Федосеев Сергей <105821791+Fedos-s@users.noreply.github.com>
2022-08-11 10:22:11 +0300

    add validation, product add in arr, animation

---
+++ src/components/AddForm.vue
@@ -0,0 +1,189 @@
+<template>
+  <aside class="add">
+    <form action="#" class="add_from form" @submit.prevent="submit">
+      <label class="form__label form__label_name">
+        <span class="form__title">Наименование товара</span>
+        <input
+          class="form__input"
+          :class="{ 'input-error': errors.nameErr }"
+          type="text"
+          name="name"
+          placeholder="Введите наименование товара"
+          required
+          v-model="product.name"
+        />
+        <span class="form__error" v-show="errors.nameErr">
+          {{ errors.nameErr }}
+        </span>
+      </label>
+      <label class="form__label form__label_about">
+        <span class="form__title">Описание товара</span>
+        <textarea
+          class="form__textarea form__input"
+          name="about"
+          placeholder="Введите наименование товара"
+          v-model="product.about"
+        ></textarea>
+      </label>
+      <label class="form__label form__label_link">
+        <span class="form__title">Ссылка на изображение товара</span>
+        <input
+          class="form__input"
+          :class="{ 'input-error': errors.linkErr }"
+          type="text"
+          name="link"
+          placeholder="Введите ссылку"
+          required
+          v-model="product.link"
+        />
+        <span class="form__error" v-show="errors.linkErr">
+          {{ errors.linkErr }}
+        </span>
+      </label>
+      <label class="form__label form__label_price">
+        <span class="form__title">Цена товара</span>
+        <input
+          class="form__input"
+          :class="{ 'input-error': errors.priceErr }"
+          type="text"
+          name="price"
+          placeholder="Введите цену"
+          required
+          v-model.number="product.price"
+        />
+        <span class="form__error" v-show="errors.priceErr">
+          {{ errors.priceErr }}
+        </span>
+      </label>
+      <transition name="added" mode="out-in">
+        <button v-if="!addProduct"
+          class="form__btn"
+          :class="{ 'btn-active': anyErrors }"
+          :disabled="!anyErrors"
+          type="submit"
+        >
+          Добавить товар
+        </button>
+        <div v-else>
+            Товар успешно добавлен
+        </div>
+      </transition>
+    </form>
+  </aside>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: {
+        name: "",
+        about: "",
+        link: "",
+        price: null,
+      },
+
+      errors: {
+        nameErr: null,
+        linkErr: null,
+        priceErr: null,
+      },
+
+      send: false,
+      addProduct: false,
+    };
+  },
+  props: ["newProduct"],
+  methods: {
+    checkName() {
+      if (this.product.name) {
+        this.product.name.length < 3
+          ? (this.errors.nameErr = "Имя не может быть меньше 3 символов")
+          : (this.errors.nameErr = null);
+      } else this.errors.nameErr = null;
+    },
+    checkLink() {
+      if (this.product.link) {
+        try {
+          new URL(this.product.link);
+          this.errors.linkErr = null;
+        } catch (error) {
+          this.errors.linkErr = "Неверная ссылка";
+        }
+      } else this.errors.linkErr = null;
+    },
+    checkPrice() {
+      if (this.product.price) {
+        this.product.price > 1
+          ? (this.errors.priceErr = null)
+          : (this.errors.priceErr = "Введено занчение меньше 1");
+      } else this.errors.priceErr = null;
+    },
+    submit() {
+        this.addProduct = true;
+      this.$emit("update:newProduct", this.product);
+      setTimeout(() => {
+        this.reset();
+      }, 0);
+      setTimeout(() => {
+        this.addProduct = false;
+      }, 1000);
+    },
+    reset() {
+      this.errors.nameErr = null;
+      this.errors.priceErr = null;
+      this.errors.linkErr = null;
+      this.product.price = null;
+      this.product.name = "";
+      this.product.link = "";
+      this.product.about = "";
+    },
+  },
+  computed: {
+    price() {
+      return this.product.price;
+    },
+    name() {
+      return this.product.name;
+    },
+    link() {
+      return this.product.link;
+    },
+    anyErrors() {
+      return (
+        ((this.errors.nameErr === null) &
+          (this.errors.priceErr === null) &
+          (this.errors.linkErr === null) &
+          (this.product.name != "") &
+          (this.product.price != null) &
+          (this.product.link != "")) |
+        this.send
+      );
+    },
+  },
+  watch: {
+    price() {
+      this.checkPrice();
+    },
+    name() {
+      this.checkName();
+    },
+    link() {
+      this.checkLink();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles.scss";
+
+.added-enter-active,
+.added-leave-active {
+  transition: all .5s;
+}
+.added-enter,
+.added-leave-to {
+  opacity: 0;
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,67 +1,139 @@
 <template>
-    <main class="main">
-        <div class="main_container content">
-            <div class="content__top">
-                <h2 class="title">Добавление товара</h2>
-                <select name="filters" id="filter-select" class="select-filter">
-                    <option class="select-placeholder" value="" disabled selected hidden>По умолчанию</option>
-                    <option value="name">А-Я</option>
-                    <option value="max">Max-Min</option>
-                    <option value="min">Min-Max</option>
-                </select>
-            </div>
-            <div class="content__catalog">
-                <aside class="add">
-                    <form action="#" class="add_from form">
-                        <label class="form__label form__label_name">
-                            <span class="form__title">Наименование товара</span>
-                            <input class="form__input" type="text" name="name" placeholder="Введите наименование товара" required>
-                        </label>
-                        <label class="form__label form__label_about">
-                            <span class="form__title">Описание товара</span>
-                            <textarea class="form__textarea form__input" name="about" placeholder="Введите наименование товара"></textarea>
-                        </label>
-                        <label class="form__label form__label_link">
-                            <span class="form__title">Ссылка на изображение товара</span>
-                            <input class="form__input" type="text" name="link" placeholder="Введите ссылку" required>
-                        </label>
-                        <label class="form__label form__label_price">
-                            <span class="form__title">Цена товара</span>
-                            <input class="form__input" type="text" name="price" placeholder="Введите цену" required>
-                        </label>
-                        <button class="form__btn btn-active" type="submit">Добавить товар</button>
-                    </form>
-                </aside>
-                <section class="catalog">
-                    <ul class="catalog__list">
-                        <li class="catalog__item">
-                            <a href="#" class="catalog__delete-btn active">
-                                <svg class="icon">
-                                   <use xlink:href="#delete-btn"></use>
-                                </svg>
-                            </a>
-                            <a class="catalog__link" href="#">
-                                <img class="catalog__pic" src="@/assets/img.jpg" alt="Название товара">
-                                <div class="catalog__bottom-section">
-                                    <h3 class="catalog__title">                   
-                                        Наименование товара
-                                    </h3>
-                                    <p class="catalog__text">
-                                        Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк
-                                    </p>
-                                    <span class="catalog__price">
-                                        10 000 ₽
-                                    </span>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-            </div>
-        </div>
-    </main>
+  <main class="main">
+    <div class="main_container content">
+      <div class="content__top">
+        <h2 class="title">Добавление товара</h2>
+        <select name="filters" id="filter-select" class="select-filter">
+          <option class="select-placeholder" value="" disabled selected hidden>
+            По умолчанию
+          </option>
+          <option value="name">А-Я</option>
+          <option value="max">Max-Min</option>
+          <option value="min">Min-Max</option>
+        </select>
+      </div>
+      <div class="content__catalog">
+        <AddForm :newProduct.sync="newProduct" />
+
+        <section class="catalog">
+          <transition-group name="product" tag="ul" class="catalog__list">
+            <li
+              class="catalog__item"
+              v-for="product in products"
+              :key="product.id"
+            >
+              <a
+                href="#"
+                class="catalog__delete-btn active"
+                @click.prevent="removeProduct(product.id)"
+              >
+                <svg class="icon">
+                  <use xlink:href="#delete-btn"></use>
+                </svg>
+              </a>
+              <a class="catalog__link" href="#">
+                <img
+                  class="catalog__pic"
+                  :src="product.link"
+                  :alt="product.name"
+                />
+                <div class="catalog__bottom-section">
+                  <h3 class="catalog__title">{{ product.name }}</h3>
+                  <p class="catalog__text">
+                    {{ product.about }}
+                  </p>
+                  <span class="catalog__price">
+                    {{ product.price | PriceFormat }} ₽
+                  </span>
+                </div>
+              </a>
+            </li>
+          </transition-group>
+        </section>
+      </div>
+    </div>
+  </main>
 </template>
 
+<script>
+import AddForm from "@/components/AddForm.vue";
+import PriceFormat from "@/filters/PriceFormat";
+
+export default {
+  data() {
+    return {
+      newProduct: null,
+      products: [
+        {
+          id: 1,
+          name: "Наименование товара",
+          about:
+            "Довольно-таки интересное описание товара в несколько строк.Довольно-таки интересное описание товара в несколько строк",
+          link: "https://randomwordgenerator.com/img/picture-generator/55e5dd474d5baf14f1dc8460962e33791c3ad6e04e507440772d73d49745c0_640.jpg",
+          price: 10000,
+        },
+        {
+          id: 2,
+          name: "Наименование товара",
+          about:
+            "Довольно-таки интересное описание товара в несколько строк.Довольно-таки интересное описание товара в несколько строк",
+          link: "https://randomwordgenerator.com/img/picture-generator/52e8dc46435aac14f1dc8460962e33791c3ad6e04e507440762e79d79e4bc1_640.jpg",
+          price: 10000,
+        },
+        {
+          id: 3,
+          name: "Наименование товара",
+          about:
+            "Довольно-таки интересное описание товара в несколько строк.Довольно-таки интересное описание товара в несколько строк",
+          link: "https://randomwordgenerator.com/img/picture-generator/55e7dc434957a814f1dc8460962e33791c3ad6e04e507440742a7ad6964bc1_640.jpg",
+          price: 10000,
+        },
+      ],
+    };
+  },
+  components: { AddForm },
+  filters: { PriceFormat },
+  methods: {
+    addNewProduct() {
+      if (this.newProduct) {
+        this.products.push({
+          id:
+            Math.max.apply(
+              Math,
+              this.products.map((e) => e.id)
+            ) + 1,
+          name: this.newProduct.name,
+          about: this.newProduct.about,
+          link: this.newProduct.link,
+          price: this.newProduct.price,
+        });
+      }
+    },
+    removeProduct(id) {
+      this.products.splice(
+        this.products.findIndex((e) => e.id === id),
+        1
+      );
+    },
+  },
+  watch: {
+    newProduct() {
+      this.addNewProduct();
+      this.newProduct = null;
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-@import '@/assets/styles.scss';
+@import "@/assets/styles.scss";
+// Animation
+.product-enter-active,
+.product-leave-active {
+  transition: opacity 5s;
+}
+.product-enter,
+.product-leave-to {
+  opacity: 0;
+}
 </style>
